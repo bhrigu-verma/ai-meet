74b2f0f0fbd866b1a22a6899f2f690889170e864
Bhrigu verma <146697095+bhrigu-verma@users.noreply.github.com>
2025-06-13 17:06:36 +0530

    configuring authentiaction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+
+"use client";
+import React from "react";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import {useState} from "react";
 
 export default function Home() {
+ const[name,setName]=useState("");
+const[email,setEmail]=useState("");
+const[password,setPassword]=useState("");
+const[confirmPassword,setConfirmPassword]=useState("");
+
+ 
   return (
+
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
