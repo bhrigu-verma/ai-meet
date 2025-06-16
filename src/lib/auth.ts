40e78e3f58afde616a48e09bdb4299511bb930dc
Bhrigu verma <146697095+bhrigu-verma@users.noreply.github.com>
2025-06-16 10:31:11 +0530

    configuring authentiaction

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,7 +6,7 @@ import * as  schema  from "@/database/schema";
 // This file sets up authentication using BetterAuth with a Drizzle ORM adapter for PostgreSQL.
 // It imports the necessary modules and initializes the auth configuration.
 export const auth = betterAuth({
-    database: drizzleAdapter(db, 
+    database: drizzleAdapter(db, {}
         provider:"pg",
         schema:{...schema,},
 }),})
